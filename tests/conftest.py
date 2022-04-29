7524d54f3e018b3263497c15d76c9a868e731ea3
Kavitha <kavita.kannanunny@gmail.com>
2022-04-29 16:34:46 -0400

    updating testcases

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,7 +31,7 @@ def application():
 def add_user(application):
     with application.app_context():
         #new record
-        user = User('keith@webizly.com', 'testtest')
+        user = User('testuser@email.com', 'testtest')
         db.session.add(user)
         db.session.commit()
 
